21b40724f11f0c7b01071bf8268aa832c7996dd0
Erich Augusto Natal da Silva <erichnatal16@gmail.com>
2026-03-25 21:47:10 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,43 @@
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre usuario={props.nome} funcao={props.cargo} anos={props.idade}/>
+      <Sociais linkedin={props.linkedin} github={props.github}/>
+       <hr/>
+    </div>
+   
+  )
+}
+
+const Sobre = (props) => {
+  return(
+    <div>
+      <h2>Olá, eu sou o {props.usuario}</h2>
+      <p>Sou {props.funcao} e tenho {props.anos} anos</p>
+    </div>
+  )
+}
+
+const Sociais = (props) => {
+  return (
+    <div>
+      <h3>Redes sociais:</h3>
+      <ul>
+        <li><a href="https://br.linkedin.com/in/erichnatal">Linkedin</a></li>
+        <li><a href="https://github.com/erichnatal">Github</a></li>
+      </ul>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe nome="Erich" cargo="desenvolvedor" idade="22" linkedin="https://br.linkedin.com/in/erichnatal" github="https://github.com/erichnatal"/>
+      <Equipe nome="Igor" cargo="desenvolvedor" idade="20" />
+    </div>
+  );
+}
+
+export default App;
